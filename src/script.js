0f97f5ff45c8fae602f5507c068e1d3778c7e295
MiguelDeTrool <migueldetrool@gmail.com>
2022-05-11 23:29:53 +0200

    Add form submit function, add form validation, refactor read status toggle so it is on Book prototype

--- src/script.js
+++ src/script.js
@@ -8,11 +8,6 @@ let myLibrary = {
         myLibrary.books.splice(bookIndex, 1);
         refreshDisplay(myLibrary, bookDisplay);
     },
-    toggleRead : function(e) {
-        let bookIndex = e.target.parentNode.parentNode.getAttribute("data-attribute"); //get data-attribute from row, which is two parents up
-        myLibrary.books[bookIndex].read = !myLibrary.books[bookIndex].read;
-        refreshDisplay(myLibrary, bookDisplay);
-    },
 };
 
 const bookDisplay = document.querySelector(".book-display");
@@ -26,6 +21,11 @@ function Book(title, author, pages, read) {
 
 Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ` + (this.read ? "read." : "not read yet."); //This line will only work if I can use ternary operator and concatenation after a return like this. Might need parentheses. 
+};
+
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+    refreshDisplay(myLibrary, bookDisplay);
 };
 
 function clearTable(table) {
@@ -45,7 +45,9 @@ function refreshDisplay(library, table) {
                 if (key === "read") { //special case for "Read" cell, to add toggle read status button 
                     let readButton = document.createElement("button");
                     readButton.textContent = `${book[key] ? "Read" : "Not Read"}`;
-                    readButton.addEventListener("click", library.toggleRead);
+                    readButton.addEventListener("click", function() {
+                        library.books[index].toggleRead();
+                    });
                     dataCell.appendChild(readButton);
                 } else dataCell.textContent = book[key];
                 bookRow.appendChild(dataCell);
@@ -65,19 +67,35 @@ function refreshDisplay(library, table) {
     });
 };
 
+let formContainer = document.querySelector(".add-book-form-container");
+
 let newBookButton = document.querySelector(".new-book-button-container>button");
 newBookButton.addEventListener("click", function() {
-    document.querySelector(".add-book-form-container").style.display = "flex";
+    formContainer.style.display = "flex";
 });
 
 let closeFormButton = document.querySelector(".close-button");
 closeFormButton.addEventListener("click", function() {
-    document.querySelector(".add-book-form-container").style.display = "none";
+    formContainer.style.display = "none";
+    //also needs to clear form fields
 });
 
-let addBookForm = document.querySelector("form");
-addBookForm.addEventListener("submit", function(e) {
-    console.log(e);
+let bookAddForm = document.querySelector("form");
+bookAddForm.addEventListener("submit", function(e) {
+    e.preventDefault(); // I think this is redundant because the form's action is javascript:void(0)
+    let title = bookAddForm.elements["title"].value;
+    bookAddForm.elements["title"].value = '';
+    let author = bookAddForm.elements["author"].value;
+    bookAddForm.elements["author"].value = '';
+    let pages = bookAddForm.elements["pages"].value;
+    bookAddForm.elements["pages"].value = '';
+    let read = bookAddForm.elements["read"].checked;
+    bookAddForm.elements["read"].checked = "true";
+
+    myLibrary.addBookToLibrary(title, author, pages, read);
+
+    formContainer.style.display = "none";
+    refreshDisplay(myLibrary, bookDisplay);
 });
 
 myLibrary.addBookToLibrary("AART", "Hank Green", 392, true);
